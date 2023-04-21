f52f58bcb921e4e7787e11715c8f25edd30cc836
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-04-21 23:47:27 +0530

    Create code.js

---
+++ Equal to 42 or Not/code.js
@@ -0,0 +1,17 @@
+var flag=false;
+for(var i=0;i<arr.length;i++)
+{
+    if(arr[i]==42)
+    {
+        flag=true;
+        break;
+    }
+}
+if(flag)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+}
